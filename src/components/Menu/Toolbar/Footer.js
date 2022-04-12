1ee51f1e24b79a19d800ea916aa0e0cf1646b979
Bas Mutsaers <bas@mutsaers.nu>
2022-04-12 11:23:50 +0200

    changed ad code

--- src/components/Menu/Toolbar/Footer.js
+++ src/components/Menu/Toolbar/Footer.js
@@ -11,17 +11,6 @@ class Footer extends Component {
                     <span> Rolstoelhockey</span>
                 </div>
                 <div> 
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6534700259705406"
-                    crossorigin="anonymous"></script>
-                <ins class="adsbygoogle"
-                    style={{display: 'block'}}
-                    data-ad-client="ca-pub-6534700259705406"
-                    data-ad-slot="4972479679"
-                    data-ad-format="auto"
-                    data-full-width-responsive="true"></ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
                 </div>
             </footer>
         );
