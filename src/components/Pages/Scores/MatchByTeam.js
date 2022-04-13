b92b3d146702715c60f2b6ffbfa0ab560da9ada5
Bas Mutsaers <bas@mutsaers.nu>
2022-04-13 16:44:36 +0200

    fix?

--- src/components/Pages/Scores/MatchByTeam.js
+++ src/components/Pages/Scores/MatchByTeam.js
@@ -32,7 +32,6 @@ class MatchByTeam extends Component {
 
   getMatches(team) {
     axios.get(`https://rolstoelhockey-backend.herokuapp.com/matches/findmatchByTeam/` + team).then(response => {
-      console.log(response.data);
       this.setState({ matches: response.data });
     });
   }
@@ -87,7 +86,7 @@ class MatchByTeam extends Component {
     const matchItems = this.state.matches.map((match, _id) => {
         return (
           <div className="subtitle">
-            {match.gameday_info.gamedate}
+            {match}
             <div className="match-group">
               <div className="match-time">{match.played_at}</div>
               <div className="match-teama">{match.teamA}</div>
