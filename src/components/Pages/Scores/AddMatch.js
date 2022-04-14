c618cdc5930f21c47a6bf5e106dba81735bc6da5
Bas Mutsaers <bas@mutsaers.nu>
2022-04-14 16:46:23 +0200

    bla

--- src/components/Pages/Scores/AddMatch.js
+++ src/components/Pages/Scores/AddMatch.js
@@ -232,7 +232,7 @@ class AddMatch extends Component {
                         </div>
                         <div className="form-group" >
                             <label>Competitiedag: </label>
-                            <select className="custom-select" value={this.state.gamedayid} onChange={(e) => { this.setState({ gamedayid: e.target.value, competitionid: e.target.competition }); console.log('select competition: ' + e.target) }}>
+                            <select className="custom-select" value={this.state.gamedayid} onChange={(e) => { this.setState({ gamedayid: e.target.value, competitionid: e.target.competition }); console.log('select competition: ' + e.target.competition) }}>
                                 {this.state.gamedays.map(gameday => (
                                     <option
                                     key={gameday.value}
